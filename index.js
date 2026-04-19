2a2005606ce21dd2bfc52c82b372ef02f2f7d4c5
allinuxuser <165105879+allinuxuser@users.noreply.github.com>
2026-04-19 16:00:20 +0800

    Add operator functions and operate function

---
+++ index.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+let num1;
+let num2;
+let operator;
+
+function operate(func, a, b) {
+  func(a, b)
+}
