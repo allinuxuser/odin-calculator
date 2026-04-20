27337f18fc18583f42337ba705eb8f3f214d911a
allinuxuser <165105879+allinuxuser@users.noreply.github.com>
2026-04-20 12:56:11 +0800

    Add non-functioning screen and buttons

--- index.js
+++ index.js
@@ -21,3 +21,30 @@ let operator;
 function operate(func, a, b) {
   func(a, b)
 }
+
+const buttonContainer = document.createElement("div");
+
+const container = document.querySelector("#container");
+
+const screen = document.createElement("input");
+const addBtn = document.createElement("button");
+const subtractBtn = document.createElement("button");
+const divideBtn = document.createElement("button");
+const multiplyBtn = document.createElement("button");
+const clearBtn = document.createElement("button");
+
+addBtn.textContent = "Add";
+subtractBtn.textContent = "Subtract";
+divideBtn.textContent = "Divide";
+multiplyBtn.textContent = "Multiply";
+clearBtn.textContent = "Clear";
+
+
+
+container.appendChild(screen);
+container.appendChild(buttonContainer);
+buttonContainer.appendChild(addBtn);
+buttonContainer.appendChild(subtractBtn);
+buttonContainer.appendChild(multiplyBtn);
+buttonContainer.appendChild(divideBtn);
+buttonContainer.appendChild(clearBtn);
